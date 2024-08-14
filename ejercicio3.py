7c49b4aebfd9bae680aced3f62a7855b7f2d2562
VedoyaJuan <juanpa.vedoya@gmail.com>
2024-08-14 18:16:22 -0300

    ejercicio3

---
+++ ejercicio3.py
@@ -0,0 +1,35 @@
+def suma ():
+    print("Suma\n")
+    num1 = int(input("\nIngrese el primer número: "))
+    num2 = int(input("\nIngrese el segundo número: "))
+    res = num1 + num2 
+    print("\nResultado: " + str(res))
+
+def resta ():
+    print("Resta\n")
+    num1 = int(input("\nIngrese el primer número: "))
+    num2 = int(input("\nIngrese el segundo número: "))
+    res = num1 - num2 
+    print("\nResultado: " + str(res))
+
+def multiplicacion ():
+    print("Multiplicacion\n")
+    num1 = int(input("\nIngrese el primer número: "))
+    num2 = int(input("\nIngrese el segundo número: "))
+    res = num1 * num2 
+    print("\nResultado: " + str(res))
+
+def division ():
+    print("Division\n")
+    num1 = int(input("\nIngrese el primer número: "))
+    num2 = int(input("\nIngrese el segundo número: "))
+    res = num1 / num2 
+    print("\nResultado: " + str(res))
+
+suma()
+resta()
+multiplicacion()
+division()
+
+
+
